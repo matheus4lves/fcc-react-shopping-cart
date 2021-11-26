bfbcd1b5de61c598dafad3d3b9ac901622bce005
Matheus Alves <matheusalves.bmd@gmail.com>
2021-11-25 22:48:35 -0300

    Add the shopping cart icon
    Add the shopping cart icon and let it be used to open the shopping cart.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import {useQuery} from "react-query"
 // Components
 import Item from "./Item/Item"
-import { Grid, LinearProgress } from "@material-ui/core"
+import { Drawer, Grid, LinearProgress } from "@material-ui/core"
 // Styles
-import {Wrapper} from "./App.styles"
+import {Wrapper, StyledButton} from "./App.styles" // Object destructuring
+import { useState } from "react"
+import Badge from "@material-ui/core/Badge"
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart"
 
 export type CartItemType = {
   id: number;
@@ -20,16 +23,27 @@ const getProducts = async (): Promise<CartItemType[]> => await (await fetch("htt
 
 function App() {
   const {data, isLoading, error} = useQuery<CartItemType[]>("products", getProducts)
-  console.log(data)
+
+  const [cartOpen, setCartOpen] = useState(false)
+  const [cartItems, setCartItems] = useState([] as CartItemType[])
 
   if(isLoading) return <LinearProgress />
 
   if(error) return <div>Something went wrong...</div>
 
   const addToCart = () => null;
+  const getTotalItems = (items: CartItemType[]) => null;
 
   return (
     <Wrapper>
+      <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
+        Cart goes here...
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color="error">
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
       <Grid container spacing={3}>
         {data?.map(item => (
           <Grid item key={item.id} xs={12} sm={4}>
