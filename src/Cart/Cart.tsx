1f50895792bf1e09d943d649dafcbf875ac7696b
Matheus Alves <matheusalves.bmd@gmail.com>
2021-11-28 14:14:57 -0300

    Display the total amount to be paid

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -11,6 +11,8 @@ type Props = {
 }
 
 const Cart: React.FC<Props> = ({cartItems, addToCart, removeFromCart}) => {
+  const calculateTotal = (items: CartItemType[]) => items.reduce((ack, item) => ack + item.amount * item.price, 0)
+
   return(
     <Wrapper>
       <h2>Your Shopping Cart</h2>
@@ -18,6 +20,7 @@ const Cart: React.FC<Props> = ({cartItems, addToCart, removeFromCart}) => {
       {cartItems.map(item => (
         <CartItem key={item.id} item={item} addToCart={addToCart} removeFromCart={removeFromCart}/>
       ))}
+      <h3>Total: ${calculateTotal(cartItems).toFixed(2)}</h3>
     </Wrapper>
   )
 }
